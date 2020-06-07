96d842e81d3dbff965f0903f3a1431f1c2aadb78
unknown <fabianbusiness@outlook.com>
2020-06-07 10:55:50 +0200

    Create music_player.py

---
+++ classifier/music_player.py
@@ -0,0 +1,21 @@
+﻿import pygame
+
+
+class MusicPlayer():
+    def __init__(self):
+        self.p = None
+        self.is_playing = False
+
+    def play(self):
+        if not self.is_playing:
+            print("Playing music...")
+            pygame.init()
+            pygame.mixer.music.load('audio.mp3')
+            pygame.mixer.music.play()
+            self.is_playing = True
+
+    def stop(self):
+        if self.is_playing:
+            print("Stopping music...")
+            pygame.mixer.music.stop()
+            self.is_playing = False
