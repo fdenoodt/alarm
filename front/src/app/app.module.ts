a5b28b42ffbee89ff7215e00e4f84fcba23de846
unknown <fabianbusiness@outlook.com>
2020-06-21 14:05:35 +0200

    temp

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
