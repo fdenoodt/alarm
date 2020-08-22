abaa3ebffe081e3bcde21b4da32af6575258a15e
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2020-08-22 14:56:58 +0200

    bugfixes

--- classifier/create_models/use_model.py
+++ classifier/create_models/use_model.py
@@ -5,13 +5,13 @@ import tensorflow as tf
 IMAGE_W = 440
 IMAGE_H = 380
 
-im = imread('/data/b.jpg', IMREAD_COLOR)
+im = imread('../data/b.jpg', IMREAD_COLOR)
 
-im = transform_image.crop(im)
-im = transform_image.reshape(im, IMAGE_W, IMAGE_H)
+im = transform_image.resize(im)
+im = transform_image.reshape(im)
 
-model = tf.keras.models.load_model('../models/cnn-128x1-1591370377.727606.model')
-# model = tf.keras.models.load_model('models/cnn-128x1-1591360990.327407.model')
+model = tf.keras.models.load_model('../models/v8-128x1.model')
 
 predictions = model.predict([im])
+print('predicted value...')
 print(predictions[0])
