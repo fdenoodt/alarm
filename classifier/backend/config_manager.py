2aa7bd9286475d1c6da68c2237c6b70256c3df2e
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2020-09-01 15:35:42 +0200

    Prepare data based on config file

--- classifier/backend/config_manager.py
+++ classifier/backend/config_manager.py
@@ -1,6 +1,9 @@
 class ConfigManager:
     import json
-
+    '''
+    WARNING: the library looks at the pwd of the script that executes it!
+    So if parent dir calls child dir ConfigManager, CM will look for config.json in parent dir
+    '''
     with open('config.json') as json_file:
         data = json.load(json_file)
 
--- classifier/create_models/prepare_data.py
+++ classifier/create_models/prepare_data.py
@@ -3,13 +3,17 @@ from PIL import Image
 from backend import transform_image
 import random
 import imagehash
-import numpy as np
+import pickle
+import cv2
+from backend.config_manager import ConfigManager as ConfManager
 
 DATA_DIR = '../data/'
 LABELS = ['in', 'out']
 
-IMAGE_W = 352
-IMAGE_H = 352
+conf = ConfManager.get_config()['data']
+
+IMAGE_W = conf['image_width']
+IMAGE_H = conf['image_height']
 
 image_data = []
 image_data_count = 0
@@ -18,15 +22,23 @@ image_data_count = 0
 for label in LABELS:
     path = os.path.join(DATA_DIR, label)
     for im in os.listdir(path):
-        image = Image.open(os.path.join(path, im))
+        image_path = os.path.join(path, im)
+        image = Image.open(image_path)  # Use of PIL for hashing later
         hash_code = imagehash.average_hash(image)
-        image_data.append([image, LABELS.index(label), hash_code])
+
+        image_data.append(
+            [
+                image_path,
+                LABELS.index(label),
+                hash_code
+            ]
+        )
         image_data_count = image_data_count + 1
 
 
 ##
 
-def otherContainHash(current_hash_code, current_index):
+def other_contain_hash(current_hash_code, current_index):
     contains = False
 
     for i in range(current_index - 5, current_index + 5):
@@ -43,35 +55,38 @@ def otherContainHash(current_hash_code, current_index):
     return contains
 
 
+# Handle duplicates
 index = 0
+training_data = []  # image, label
 for data in image_data:
     hash_code = data[2]
 
-    if otherContainHash(hash_code, index):
-        data.append(False)
-    else:  # these are unique
-        data.append(True)
+    # if remove duplicates = True -> check whether unique or not
+    if not conf['remove_duplicates'] or not other_contain_hash(hash_code, index):
+        label = data[1]
+        cv_image = cv2.imread(data[0])
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        cv_image = transform_image.resize(cv_image)
+        training_data.append([cv_image, label])
 
     index = index + 1
 
-image_data = list(filter(lambda x: x[3] == True, image_data))  # only keep unique images
-training_data = list(map(lambda x: [np.asarray(x[0]), x[1]], image_data))  # transform PIL Image to numpy array
-
 # Spread data evenly
-ins = list(filter(lambda x: x[1] == LABELS.index('in'), training_data))
-outs = list(filter(lambda x: x[1] == LABELS.index('out'), training_data))
+if conf['spread_evenly']:
+    ins = list(filter(lambda x: x[1] == LABELS.index('in'), training_data))
+    outs = list(filter(lambda x: x[1] == LABELS.index('out'), training_data))
 
-target_length = len(outs) if len(outs) < len(ins) else len(ins)
+    target_length = len(outs) if len(outs) < len(ins) else len(ins)
 
-random.shuffle(ins)
-random.shuffle(outs)
+    random.shuffle(ins)
+    random.shuffle(outs)
 
-percent_data = 1  # 100%
-ins = ins[:int(target_length * percent_data)]
-outs = outs[:int(target_length * percent_data)]
+    percent_data = 1  # 100%
+    ins = ins[:int(target_length * percent_data)]
+    outs = outs[:int(target_length * percent_data)]
 
-training_data = outs
-training_data.extend(ins)
+    training_data = outs
+    training_data.extend(ins)
 
 random.shuffle(training_data)
 
@@ -81,23 +96,17 @@ xs = []
 ys = []
 
 for features, label in training_data:
-    features = transform_image.resize(features)
     xs.append(features)
     ys.append(label)
 
+
 xs = transform_image.reshape(xs)
 
-##
-
-import pickle
-
 # Save images
-pickle_out = open("../data/X8.pickle", "wb")
+pickle_out = open("../data/X{}.pickle".format(conf['version']), "wb")
 pickle.dump(xs, pickle_out)
 pickle_out.close()
 
-pickle_out = open("../data/Y8.pickle", "wb")
+pickle_out = open("../data/Y{}.pickle".format(conf['version']), "wb")
 pickle.dump(ys, pickle_out)
 pickle_out.close()
-
-##
